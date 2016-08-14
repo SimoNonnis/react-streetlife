b6c7df9fb01b304c5c87e4c96d96b726c01ef31b
Simon Nonnis <simononnis@gmail.com>
2016-08-14 11:24:10 +0200

    'Remove clean.js'

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import clean from '../helpers/clean';
 
 import styles from './post.css';
 
 const Post = ({title, body}) => {
-  var cleanedBody = clean(body);
-  //console.log(body);
-
   return (
     <div className={styles.container} >
       <h2 className={styles.title} >{title}</h2>
-      <div>{cleanedBody}</div>
+      <div>{body}</div>
     </div>
   );
 };

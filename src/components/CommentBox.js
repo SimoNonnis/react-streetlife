1badcb9180095348cd9cae11cda8b30160011311
Simon Nonnis <simononnis@gmail.com>
2016-08-16 15:23:44 +0200

    'Add animation'

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import classNames from 'classnames/bind';
 
 import Comment from './Comment';
 
 import styles from './commentBox.css';
+let cx = classNames.bind(styles);
 
 
 const CommentBox = ({comments, commentsCount, postId, allComments, getAllComments, viewAll}) => {
+
+  var transitionClasses = cx({
+    enter: styles.enter,
+    enterActive: styles.enterActive,
+    leave: styles.leave,
+    leaveActive: styles.leaveActive
+  });
+
   return (
     <div className={styles.container}>
       <h4 className={styles.title}>Latest Comments</h4>
@@ -16,28 +27,40 @@ const CommentBox = ({comments, commentsCount, postId, allComments, getAllComment
       >{ !viewAll ? 'View All' : 'Hide All'}
         <span className={styles.counter}>{commentsCount}</span>
       </button>
-      { comments.map((comment) => {
-        return (
-          <Comment
-            key={comment.id}
-            avatarUrl={comment.author.avatar}
-            name={comment.author.display_name}
-            body={comment.body}
-            time={comment.updated_at}
-          />
-        );
-      }) }
-      { viewAll ? allComments.map((comment) => {
-        return (
-          <Comment
-            key={comment.id}
-            avatarUrl={comment.author.avatar}
-            name={comment.author.display_name}
-            body={comment.body}
-            time={comment.updated_at}
-          />
-        );
-      }) : null }
+      <div className={styles.commentsWrapper} >
+        <ReactCSSTransitionGroup
+          component='div'
+          transitionName={transitionClasses}
+          transitionEnterTimeout={5000}
+          transitionLeaveTimeout={3000}
+          className={styles.lastCommentsWrapper}
+        >
+          { comments.map((comment) => {
+            return (
+              <Comment
+                key={comment.id}
+                avatarUrl={comment.author.avatar}
+                name={comment.author.display_name}
+                body={comment.body}
+                time={comment.updated_at}
+              />
+            );
+          }) }
+        </ReactCSSTransitionGroup>
+        <div className={styles.allCommentsWrapper} >
+          { viewAll ? allComments.map((comment) => {
+            return (
+              <Comment
+                key={comment.id}
+                avatarUrl={comment.author.avatar}
+                name={comment.author.display_name}
+                body={comment.body}
+                time={comment.updated_at}
+              />
+            );
+          }) : null }
+        </div>
+      </div>
     </div>
   );
 };
